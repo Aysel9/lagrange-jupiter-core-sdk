efa7a45b108df5cc9ec086b156891082e5433cf8
Aysel9 <89453868+Aysel9@users.noreply.github.com>
2022-02-02 10:44:09 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,44 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+
+/** eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require("next-transpile-modules")([
+  "@solana/wallet-adapter-base",
+  // Uncomment wallets you want to use
+  // "@solana/wallet-adapter-bitpie",
+  // "@solana/wallet-adapter-coin98",
+  // "@solana/wallet-adapter-ledger",
+  // "@solana/wallet-adapter-mathwallet",
+  "@solana/wallet-adapter-phantom",
+  "@solana/wallet-adapter-react",
+  // "@solana/wallet-adapter-solflare",
+  // "@solana/wallet-adapter-sollet",
+  // "@solana/wallet-adapter-solong",
+  // "@solana/wallet-adapter-torus",
+  "@solana/wallet-adapter-wallets",
+  // "@project-serum/sol-wallet-adapter",
+  // "@solana/wallet-adapter-ant-design",
+]);
+const plugins = [
+  // add this if you need LESS
+  // [withLess, {
+  //   lessLoaderOptions: {
+  //     /* ... */
+  //   },
+  // }],
+  [
+    withTM,
+    {
+      webpack5: true,
+      reactStrictMode: true,
+      images: {
+        domains: ['avatars.dicebear.com', 'raw.githubusercontent.com', 's2.coinmarketcap.com','images.unsplash.com'],
+      },
+    },
+  ],
+];
+
+
 module.exports = {
   eslint: {
     dirs: ['src'],
@@ -15,6 +55,11 @@ module.exports = {
   // },
 
   // SVGR
+
+};
+
+const nextConfig = {
+    swcMinify: false,
   webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/i,
@@ -38,4 +83,7 @@ module.exports = {
 
     return config;
   },
-};
+  }
+
+
+module.exports = withPlugins(plugins, nextConfig);
